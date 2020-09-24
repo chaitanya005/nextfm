0bdb09fd8773b73eca64b2638012fca2b89dee8f
github_chaitanyamuvvala <muvvalachaitanya05@gmail.com>
2020-09-24 17:05:01 +0530

    Add: Testing Analytics 1

--- pages/_document.js
+++ pages/_document.js
@@ -105,17 +105,15 @@ class MyDocument extends Document {
           {/*<!-- Global site tag (gtag.js) - Google Analytics -->*/}
 
           <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            <script
             async
             src="https://www.googletagmanager.com/gtag/js?id=UA-175221452-1"
           ></script>
-          <script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
             window.dataLayer = window.dataLayer || []; function gtag()
             {dataLayer.push(arguments)}
             gtag('js', new Date()); gtag('config', 'UA-175221452-1');
-          </script>
               `,
             }}
           />
